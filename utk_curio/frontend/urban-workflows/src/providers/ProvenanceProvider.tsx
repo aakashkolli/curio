33b58ca56929c23c583009560346680312ab5448
ShanmukhUpad <supad83@uic.edu>
2025-07-29 09:30:21 -0500

    Made changes to data pool box to display an input from the merge box

--- utk_curio/frontend/urban-workflows/src/providers/ProvenanceProvider.tsx
+++ utk_curio/frontend/urban-workflows/src/providers/ProvenanceProvider.tsx
@@ -44,7 +44,7 @@ export const ProvenanceContext = createContext<ProvenanceContextProps>({
 });
 
 const ProvenanceProvider = ({ children }: { children: ReactNode }) => {
-    const [provenanceGraphBoxes, _setProvenanceGraphBoxes] = useState<any>({}); // workflow_name -> activity_name -> nodes[]
+    const [provenanceGraphBoxes, _setProvenanceGraphBoxes] = useState<any>({});
     const provenanceGraphBoxesRef = React.useRef(provenanceGraphBoxes);
     const setProvenanceGraphBoxes = (data: any) => {
         provenanceGraphBoxesRef.current = data;
@@ -54,62 +54,33 @@ const ProvenanceProvider = ({ children }: { children: ReactNode }) => {
     const addUser = (user_name: string, user_type: string, user_IP: string) => {
         fetch(process.env.BACKEND_URL + "/saveUserProv", {
             method: "POST",
-            body: JSON.stringify({
-                user: {
-                    user_name,
-                    user_type,
-                    user_IP,
-                },
-            }),
-            headers: {
-                "Content-type": "application/json; charset=UTF-8",
-            },
+            body: JSON.stringify({ user: { user_name, user_type, user_IP } }),
+            headers: { "Content-type": "application/json; charset=UTF-8" },
         });
     };
 
     const newBox = (workflow_name: string, activity_name: string) => {
         fetch(process.env.BACKEND_URL + "/newBoxProv", {
             method: "POST",
-            body: JSON.stringify({
-                data: {
-                    workflow_name,
-                    activity_name,
-                },
-            }),
-            headers: {
-                "Content-type": "application/json; charset=UTF-8",
-            },
+            body: JSON.stringify({ data: { workflow_name, activity_name } }),
+            headers: { "Content-type": "application/json; charset=UTF-8" },
         });
     };
 
     const deleteBox = (workflow_name: string, activity_name: string) => {
         fetch(process.env.BACKEND_URL + "/deleteBoxProv", {
             method: "POST",
-            body: JSON.stringify({
-                data: {
-                    workflow_name,
-                    activity_name,
-                },
-            }),
-            headers: {
-                "Content-type": "application/json; charset=UTF-8",
-            },
+            body: JSON.stringify({ data: { workflow_name, activity_name } }),
+            headers: { "Content-type": "application/json; charset=UTF-8" },
         });
     };
 
     const addWorkflow = async (workflow_name: string) => {
-        await fetch(process.env.BACKEND_URL + "/truncateDBProv", {
-            method: "GET",
-        });
-
+        await fetch(process.env.BACKEND_URL + "/truncateDBProv", { method: "GET" });
         await fetch(process.env.BACKEND_URL + "/saveWorkflowProv", {
             method: "POST",
-            body: JSON.stringify({
-                workflow: workflow_name,
-            }),
-            headers: {
-                "Content-type": "application/json; charset=UTF-8",
-            },
+            body: JSON.stringify({ workflow: workflow_name }),
+            headers: { "Content-type": "application/json; charset=UTF-8" },
         });
     };
 
@@ -120,41 +91,33 @@ const ProvenanceProvider = ({ children }: { children: ReactNode }) => {
         targetNodeId: string,
         targetNodeType: BoxType
     ) => {
+        if (!workflow_name || !sourceNodeId || !sourceNodeType || !targetNodeId || !targetNodeType) {
+            console.error("[newConnection] Missing or invalid data in payload", {
+                workflow_name, sourceNodeId, sourceNodeType, targetNodeId, targetNodeType,
+            });
+            return;
+        }
 
         fetch(process.env.BACKEND_URL + "/newConnectionProv", {
             method: "POST",
             body: JSON.stringify({
-                data: {
-                    workflow_name,
-                    sourceNodeId,
-                    sourceNodeType,
-                    targetNodeId,
-                    targetNodeType,
-                },
+                data: { workflow_name, sourceNodeId, sourceNodeType, targetNodeId, targetNodeType },
             }),
-            headers: {
-                "Content-type": "application/json; charset=UTF-8",
-            },
-        });
+            headers: { "Content-type": "application/json; charset=UTF-8" },
+        })
+            .then((response) => {
+                if (!response.ok) throw new Error(`[newConnection] Failed: ${response.statusText}`);
+                return response.json();
+            })
+            .then((data) => console.log("[newConnection] Success:", data))
+            .catch((error) => console.error("[newConnection] Error:", error.message));
     };
 
-    const deleteConnection = (
-        workflow_name: string,
-        targetNodeId: string,
-        targetNodeType: BoxType
-    ) => {
+    const deleteConnection = (workflow_name: string, targetNodeId: string, targetNodeType: BoxType) => {
         fetch(process.env.BACKEND_URL + "/deleteConnectionProv", {
             method: "POST",
-            body: JSON.stringify({
-                data: {
-                    workflow_name,
-                    targetNodeId,
-                    targetNodeType,
-                },
-            }),
-            headers: {
-                "Content-type": "application/json; charset=UTF-8",
-            },
+            body: JSON.stringify({ data: { workflow_name, targetNodeId, targetNodeType } }),
+            headers: { "Content-type": "application/json; charset=UTF-8" },
         });
     };
 
@@ -180,81 +143,46 @@ const ProvenanceProvider = ({ children }: { children: ReactNode }) => {
                     activity_source_code,
                 },
             }),
-            headers: {
-                "Content-type": "application/json; charset=UTF-8",
-            },
-        }).then((value: any) => {
-            getBoxGraph(workflow_name, activity_name);
-        });
+            headers: { "Content-type": "application/json; charset=UTF-8" },
+        }).then(() => getBoxGraph(workflow_name, activity_name));
     };
 
     const getBoxGraph = (workflow_name: string, activity_name: string) => {
-        // Call after writing the running provenance in the database
-
         fetch(process.env.BACKEND_URL + "/getBoxGraph", {
             method: "POST",
-            body: JSON.stringify({
-                data: {
-                    workflow_name,
-                    activity_name,
-                },
-            }),
-            headers: {
-                "Content-type": "application/json; charset=UTF-8",
-            },
+            body: JSON.stringify({ data: { workflow_name, activity_name } }),
+            headers: { "Content-type": "application/json; charset=UTF-8" },
         })
             .then((response) => response.json())
             .then((json: any) => {
                 let newProvenanceGraphs: any = {};
                 let added = false;
 
-                let workflows = Object.keys(provenanceGraphBoxesRef.current);
-
-                for (const workflow of workflows) {
-                    if (newProvenanceGraphs[workflow] == undefined)
-                        newProvenanceGraphs[workflow] = {};
-
-                    let activities = Object.keys(
-                        provenanceGraphBoxesRef.current[workflow]
-                    );
-
-                    for (const activity of activities) {
-                        if (
-                            workflow == workflow_name &&
-                            activity == activity_name
-                        ) {
-                            newProvenanceGraphs[workflow][activity] =
-                                json["graph"];
+                for (const workflow of Object.keys(provenanceGraphBoxesRef.current)) {
+                    newProvenanceGraphs[workflow] = {};
+                    for (const activity of Object.keys(provenanceGraphBoxesRef.current[workflow])) {
+                        if (workflow === workflow_name && activity === activity_name) {
+                            newProvenanceGraphs[workflow][activity] = json["graph"];
                             added = true;
                         } else {
-                            // TODO: replicate array of objects
-                            newProvenanceGraphs[workflow][activity] =
-                                provenanceGraphBoxesRef.current[workflow][
-                                    activity
-                                ].map((obj: any) => {
-                                    return { ...obj };
-                                });
+                            newProvenanceGraphs[workflow][activity] = JSON.parse(JSON.stringify(
+                                provenanceGraphBoxesRef.current[workflow][activity]
+                            ));
                         }
                     }
                 }
 
                 if (!added) {
-                    if (newProvenanceGraphs[workflow_name] == undefined)
-                        newProvenanceGraphs[workflow_name] = {};
-
-                    newProvenanceGraphs[workflow_name][activity_name] =
-                        json["graph"];
+                    if (!newProvenanceGraphs[workflow_name]) newProvenanceGraphs[workflow_name] = {};
+                    newProvenanceGraphs[workflow_name][activity_name] = json["graph"];
                 }
 
                 setProvenanceGraphBoxes(newProvenanceGraphs);
             });
     };
 
-    // for test purposes (TODO: temporary)
     const truncateDB = () => {
-        fetch(process.env.BACKEND_URL + "/truncateDBProv", {
-            method: "GET",
-        });
+        fetch(process.env.BACKEND_URL + "/truncateDBProv", { method: "GET" });
     };
 
     return (
@@ -278,13 +206,7 @@ const ProvenanceProvider = ({ children }: { children: ReactNode }) => {
 
 export const useProvenanceContext = () => {
     const context = useContext(ProvenanceContext);
-
-    if (!context) {
-        throw new Error(
-            "useProvenanceContext must be used within a ProvenanceProvider"
-        );
-    }
-
+    if (!context) throw new Error("useProvenanceContext must be used within a ProvenanceProvider");
     return context;
 };
 
